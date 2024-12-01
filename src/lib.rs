2ea5dcb37a829dd5987c15e19d5c1a6762c8b499
elliottyoon <elliottyoon@u.northwestern.edu>
2024-11-30 23:53:12 -0500

    Initial framework

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+//! # Advent of Code solutions in Rust, hopefully.
+
+/// # Utility modules to handle common recurring Advent of Code pattern
+pub mod util {
+    pub mod integer;
+    pub mod parse;
+}
+
+/// # pls why did i agree to do this D:
+pub mod year2024 {
+    pub mod day01;
+}
---
+++ tests/year2024/day01_test.rs
@@ -0,0 +1,11 @@
+use aoc::year2024::day01;
+
+#[test]
+fn part1_test() {
+    todo!();
+}
+
+#[test]
+fn part2_test() {
+    todo!();
+}
