b55e4ca018a4aa1694d20084983944630ab54a99
elliottyoon <elliottyoon@u.northwestern.edu>
2024-12-01 08:31:51 -0600

    Sorting in-place for slight perf boost

--- src/year2024/day01.rs
+++ src/year2024/day01.rs
@@ -4,17 +4,23 @@ use crate::util::parse::ParseOps;
 type Input = (Vec<u32>, Vec<u32>);
 
 pub fn parse(input: &str) -> Input {
-    input.iter_unsigned::<u32>().chunk::<2>().map(|[l, r]| (l, r)).unzip()
+    input
+        .iter_unsigned::<u32>()
+        .chunk::<2>()
+        .map(|[l, r]| (l, r))
+        .unzip()
 }
 
 pub fn part1(input: &Input) -> u32 {
-    let mut left = input.0.clone();
-    left.sort_unstable();
+    let (mut left, mut right) = input.clone();
 
-    let mut right = input.1.clone();
+    left.sort_unstable();
     right.sort_unstable();
 
-    left.into_iter().zip(right).map(|(l, r)| l.abs_diff(r)).sum()
+    left.into_iter()
+        .zip(right)
+        .map(|(l, r)| l.abs_diff(r))
+        .sum()
 }
 
 pub fn part2(_input: &Input) -> u32 {
