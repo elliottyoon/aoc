ded4717f7c11fb5295eb997b50d32255324601ad
elliottyoon <elliottyoon@u.northwestern.edu>
2024-12-02 12:22:11 -0600

    Added benchmark framework

---
+++ README.md
@@ -0,0 +1,15 @@
+## Performance
+
+Benchmarks are measured using the built-in `cargo bench` tool run on
+an Apple M4 Pro with 24 GB of RAM.
+
+| Year          | Benchmark (ms) |
+|---------------|---------------:|
+| [2024](#2024) |  (in progress) |
+
+## 2024
+
+| Day | Problem                                                   | Solution                        | Benchmark (μs) |
+|-----|-----------------------------------------------------------|---------------------------------|---------------:|
+| 1   | [Historian Hysteria](https://adventofcode.com/2024/day/1) | [Source](src/year2024/day01.rs) |             16 |
+| 2   | [Red-Nosed Reports](https://adventofcode.com/2024/day/2)  | [Source](src/year2024/day02.rs) |             42 |
---
+++ benches/benchmark.rs
@@ -0,0 +1,50 @@
+//! Benchmark macro source: https://github.com/maneatingape/advent-of-code-rust/blob/main/benches/benchmark.rs
+
+#![allow(unstable_features)]
+#![feature(test)]
+extern crate test;
+
+macro_rules! benchmark {
+    ($year:tt, $day:tt) => {
+        mod $day {
+            use aoc::$year::$day::*;
+            use std::fs::read_to_string;
+            use std::path::Path;
+            use std::sync::LazyLock;
+            use test::Bencher;
+
+            static DATA: LazyLock<String> = LazyLock::new(|| {
+                let year = stringify!($year);
+                let day = stringify!($day);
+                let path = Path::new("input")
+                    .join(year)
+                    .join(day)
+                    .with_extension("txt");
+                read_to_string(path).unwrap()
+            });
+
+            #[bench]
+            fn parse_bench(b: &mut Bencher) {
+                let data = &DATA;
+                b.iter(|| parse(data));
+            }
+
+            #[bench]
+            fn part1_bench(b: &mut Bencher) {
+                let input = parse(&DATA);
+                b.iter(|| part1(&input));
+            }
+
+            #[bench]
+            fn part2_bench(b: &mut Bencher) {
+                let input = parse(&DATA);
+                b.iter(|| part2(&input));
+            }
+        }
+    };
+}
+
+mod year2024 {
+    benchmark!(year2024, day01);
+    benchmark!(year2024, day02);
+}
