c58fee76decc7cf17c82d2decd8bdc981a81fe46
elliottyoon <elliottyoon@u.northwestern.edu>
2024-12-03 01:28:15 -0600

    Improved performance with more cache-friendly code

--- README.md
+++ README.md
@@ -19,5 +19,5 @@ an Apple M4 Pro with 24 GB of RAM.
 | Day | Problem                                                   | Solution                        | Benchmark (μs) |
 |-----|-----------------------------------------------------------|---------------------------------|---------------:|
 | 1   | [Historian Hysteria](https://adventofcode.com/2024/day/1) | [Source](src/year2024/day01.rs) |             16 |
-| 2   | [Red-Nosed Reports](https://adventofcode.com/2024/day/2)  | [Source](src/year2024/day02.rs) |             42 |
-| 3   | [Mull It Over](https://adventofcode.com/2024/day/3)       | [Source](src/year2024/day03.rs) |            133 |
+| 2   | [Red-Nosed Reports](https://adventofcode.com/2024/day/2)  | [Source](src/year2024/day02.rs) |             39 |
+| 3   | [Mull It Over](https://adventofcode.com/2024/day/3)       | [Source](src/year2024/day03.rs) |            130 |
--- src/year2024/day03.rs
+++ src/year2024/day03.rs
@@ -1,41 +1,36 @@
 use regex::Regex;
 
-type Input = Vec<(bool, i32, i32)>;
+type Input = Vec<(bool, u32, u32)>;
 pub fn parse(input: &str) -> Input {
+    let mul_regex = Regex::new(r"^(\d+),(\d+)\)").unwrap();
     let mut skipped = false;
     let mut next_skipped = false;
 
-    let mult_regex = Regex::new(r"^(\d+),(\d+)\)").unwrap();
-
     input
         .split("mul(")
-        .filter_map(|thing| {
+        .filter_map(|split| {
             skipped = next_skipped;
-
-            let ret = mult_regex
-                .captures(thing)
-                .map(|nums| (nums[1].parse().unwrap(), nums[2].parse().unwrap()));
-
-            let dont_idx = thing.find("don't()");
-            let do_idx = thing.find("do()");
-            match (dont_idx, do_idx) {
-                (Some(dont), Some(do_)) => {
-                    next_skipped = dont > do_;
+            match (split.find("don't()"), split.find("do()")) {
+                (Some(dont_idx), Some(do_idx)) => {
+                    next_skipped = dont_idx > do_idx;
                 }
                 (Some(_), None) => next_skipped = true,
                 (None, Some(_)) => next_skipped = false,
                 _ => {}
             }
-            ret.map(|(a, b)| (skipped, a, b))
+
+            mul_regex
+                .captures(split)
+                .map(|nums| (skipped, nums[1].parse().unwrap(), nums[2].parse().unwrap()))
         })
         .collect::<Vec<_>>()
 }
 
-pub fn part1(input: &Input) -> i32 {
-    input.iter().map(|(_skipped, a, b)| a * b).sum()
+pub fn part1(input: &Input) -> u32 {
+    input.iter().map(|(_, a, b)| a * b).sum()
 }
 
-pub fn part2(input: &Input) -> i32 {
+pub fn part2(input: &Input) -> u32 {
     input
         .iter()
         .filter_map(|(skipped, a, b)| if *skipped { None } else { Some(a * b) })
